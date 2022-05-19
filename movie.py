e165ce567cfeb289463ec60820d48782a4fa1235
m-shehzad <shehzadboii@gmail.com>
2022-05-19 20:56:39 +0300

    single table

--- movie.py
+++ movie.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-# #CREATION OF DATABASE
+# CREATION OF DATABASE
 # conn = sqlite3.connect('MOVIES.db')
 
 # # CREATION OF TABLE
@@ -9,54 +9,39 @@ import sqlite3
 # c.execute("""
 # CREATE TABLE MOVIES(
 # movie_name text PRIMARY KEY,
+# lead_actor text,
+# lead_actress text,
+# director text,
 # movie_year integer
 # )
 #  """)
-
-# c.execute("""
-# CREATE TABLE MOVIE_CAST(
-#     movie_name text,
-#     actor_role text,
-#     actor_name text,
-#     PRIMARY KEY(movie_name,actor_role),
-#     FOREIGN KEY (movie_name) REFERENCES MOVIES(movie_name) ON DELETE CASCADE
-# )
-# """)
 
 # conn.commit()
 # conn.close()
 
 
-# insert function
+# insertion of movie
 def insertMovie():
     conn = sqlite3.connect("MOVIES.db")
     c = conn.cursor()
     movie_name = input("Enter the movie name: ")
+    actor_name = input("Enter the lead actor name: ")
+    actress_name = input("Enter the lead actress name: ")
+    director = input("Enter the director name: ")
     movie_year = int(input("Enter the year of release: "))
-    c.execute("INSERT INTO MOVIES (movie_name,movie_year) VALUES (?,?)",
-              (movie_name, movie_year))
-    n = int(input("Enter the number of cast members: "))
-    for i in range(1, n+1):
-        actor_role = input("Enter the role of actor "+str(i)+":")
-        actor_name = input("Enter the name of actor "+str(i)+":")
-        c.execute("INSERT INTO MOVIE_CAST (movie_name,actor_role,actor_name) VALUES (?,?,?)",
-                  (movie_name, actor_role, actor_name))
+    c.execute("INSERT INTO MOVIES (movie_name,lead_actor,lead_actress,director,movie_year) VALUES (?,?,?,?,?)",
+              (movie_name, actor_name, actress_name, director, movie_year))
     conn.commit()
     conn.close()
-    return
 
 
 def selectMovie(movieName):
     conn = sqlite3.connect("MOVIES.db")
     c = conn.cursor()
     c.execute("SELECT * FROM MOVIES WHERE movie_name = ?", (movieName,))
-    movie_details = c.fetchall()
+    movie_details = c.fetchone()
     print(movieName+" details: ")
     print(movie_details)
-    c.execute("SELECT * FROM MOVIE_CAST WHERE movie_name = ?", (movieName,))
-    cast_details = c.fetchall()
-    print(movieName + " cast:")
-    print(cast_details)
     conn.commit()
     conn.close()
 
@@ -65,18 +50,39 @@ def actorDetails(actorName):
     conn = sqlite3.connect("MOVIES.db")
     c = conn.cursor()
     c.execute("""
-    SELECT MOVIES.movie_name,movie_year,actor_role
+    SELECT *
     FROM MOVIES
-    INNER JOIN MOVIE_CAST ON
-    MOVIES.movie_name = MOVIE_CAST.movie_name
-    WHERE actor_name = ?
-    """, (actorName,))
+    WHERE lead_actor = ? OR lead_actress = ?
+    """, (actorName, actorName))
     actorMovies = c.fetchall()
     for movie in actorMovies:
         print("------------------------------")
         print("Movie name:"+movie[0])
-        print("Movie year:"+str(movie[1]))
-        print("Actor Role:"+movie[2])
+        print("Lead Actor:"+movie[1])
+        print("Lead Actress:"+movie[2])
+        print("Director: " + movie[3])
+        print("Movie year:"+str(movie[4]))
+        print("------------------------------")
+    conn.commit()
+    conn.close()
+
+
+def directorDetails(directorName):
+    conn = sqlite3.connect("MOVIES.db")
+    c = conn.cursor()
+    c.execute("""
+    SELECT *
+    FROM MOVIES
+    WHERE director = ?
+    """, (directorName,))
+    directorMovies = c.fetchall()
+    for movie in directorMovies:
+        print("------------------------------")
+        print("Movie name:"+movie[0])
+        print("Lead Actor:"+movie[1])
+        print("Lead Actress:"+movie[2])
+        print("Director: " + movie[3])
+        print("Movie year:"+str(movie[4]))
         print("------------------------------")
     conn.commit()
     conn.close()
@@ -86,8 +92,9 @@ print("~~Movie Database~~")
 while True:
     print("1. Insert a new movie")
     print("2. Select a movie")
-    print("3. show all the movies an actor has acted in")
-    print("4. Exit")
+    print("3. Display all the movies an actor/actress has starred in: ")
+    print("4. Display all the movies directed: ")
+    print("5. Exit")
     op = int(input("Enter your choice: "))
 
     if op == 1:
@@ -99,4 +106,7 @@ while True:
         actorName = input("Enter the actor Name: ")
         actorDetails(actorName)
     elif op == 4:
+        directorName = input("Enter the Director Name:")
+        directorDetails()
+    elif op == 5:
         break
