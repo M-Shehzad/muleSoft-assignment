8df69901418b7613e81dd2c37f516b02c64ea0f4
m-shehzad <shehzadboii@gmail.com>
2022-05-20 16:09:43 +0300

    error handling added

--- movie.py
+++ movie.py
@@ -1,10 +1,11 @@
+from msilib.schema import Error
 import sqlite3
 
 
 # CREATION OF DATABASE
-# conn = sqlite3.connect('MOVIES.db')
+conn = sqlite3.connect('MOVIES.db')
 
-# # CREATION OF TABLE
+# CREATION OF TABLE
 # c = conn.cursor()
 # c.execute("""
 # CREATE TABLE MOVIES(
@@ -15,85 +16,105 @@ import sqlite3
 # movie_year integer
 # )
 #  """)
-
 # conn.commit()
 # conn.close()
 
 
 # insertion of movie
 def insertMovie():
-    conn = sqlite3.connect("MOVIES.db")
-    c = conn.cursor()
-    movie_name = input("Enter the movie name: ")
-    actor_name = input("Enter the lead actor name: ")
-    actress_name = input("Enter the lead actress name: ")
-    director = input("Enter the director name: ")
-    movie_year = int(input("Enter the year of release: "))
-    c.execute("INSERT INTO MOVIES (movie_name,lead_actor,lead_actress,director,movie_year) VALUES (?,?,?,?,?)",
-              (movie_name, actor_name, actress_name, director, movie_year))
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect("MOVIES.db")
+        c = conn.cursor()
+        movie_name = input("Enter the movie name: ")
+        actor_name = input("Enter the lead actor name: ")
+        actress_name = input("Enter the lead actress name: ")
+        director = input("Enter the director name: ")
+        movie_year = int(input("Enter the year of release: "))
+        c.execute("INSERT INTO MOVIES (movie_name,lead_actor,lead_actress,director,movie_year) VALUES (?,?,?,?,?)",
+                  (movie_name.lower(), actor_name.lower(), actress_name.lower(), director.lower(), movie_year))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(e)
 
 
 def selectMovie(movieName):
-    conn = sqlite3.connect("MOVIES.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM MOVIES WHERE movie_name = ?", (movieName,))
-    movie_details = c.fetchone()
-    print(movieName+" details: ")
-    print(movie_details)
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect("MOVIES.db")
+        c = conn.cursor()
+        c.execute(
+            "SELECT * FROM MOVIES WHERE movie_name LIKE ?", ('%'+movieName.lower()+'%',))
+        movie_details = c.fetchall()
+        print("Results for "+movieName+":")
+        for movie in movie_details:
+            print("------------------------------")
+            print("Movie name:"+movie[0])
+            print("Lead Actor:"+movie[1])
+            print("Lead Actress:"+movie[2])
+            print("Director: " + movie[3])
+            print("Movie year:"+str(movie[4]))
+            print("------------------------------")
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(e)
 
 
 def actorDetails(actorName):
-    conn = sqlite3.connect("MOVIES.db")
-    c = conn.cursor()
-    c.execute("""
-    SELECT *
-    FROM MOVIES
-    WHERE lead_actor = ? OR lead_actress = ?
-    """, (actorName, actorName))
-    actorMovies = c.fetchall()
-    for movie in actorMovies:
-        print("------------------------------")
-        print("Movie name:"+movie[0])
-        print("Lead Actor:"+movie[1])
-        print("Lead Actress:"+movie[2])
-        print("Director: " + movie[3])
-        print("Movie year:"+str(movie[4]))
-        print("------------------------------")
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect("MOVIES.db")
+        c = conn.cursor()
+        c.execute("""
+        SELECT *
+        FROM MOVIES
+        WHERE lead_actor LIKE ? OR lead_actress LIKE ?
+        """, ('%'+actorName+'%', '%'+actorName+'%'))
+        actorMovies = c.fetchall()
+        print(f"All the movies {actorName} has acted in:")
+        for movie in actorMovies:
+            print("------------------------------")
+            print("Movie name:"+movie[0])
+            print("Lead Actor:"+movie[1])
+            print("Lead Actress:"+movie[2])
+            print("Director: " + movie[3])
+            print("Movie year:"+str(movie[4]))
+            print("------------------------------")
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(e)
 
 
 def directorDetails(directorName):
-    conn = sqlite3.connect("MOVIES.db")
-    c = conn.cursor()
-    c.execute("""
-    SELECT *
-    FROM MOVIES
-    WHERE director = ?
-    """, (directorName,))
-    directorMovies = c.fetchall()
-    for movie in directorMovies:
-        print("------------------------------")
-        print("Movie name:"+movie[0])
-        print("Lead Actor:"+movie[1])
-        print("Lead Actress:"+movie[2])
-        print("Director: " + movie[3])
-        print("Movie year:"+str(movie[4]))
-        print("------------------------------")
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect("MOVIES.db")
+        c = conn.cursor()
+        c.execute("""
+        SELECT *
+        FROM MOVIES
+        WHERE director LIKE ?
+        """, ('%'+directorName.lower()+'%',))
+        directorMovies = c.fetchall()
+        for movie in directorMovies:
+            print("------------------------------")
+            print("Movie name:"+movie[0])
+            print("Lead Actor:"+movie[1])
+            print("Lead Actress:"+movie[2])
+            print("Director: " + movie[3])
+            print("Movie year:"+str(movie[4]))
+            print("------------------------------")
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(e)
 
 
-print("~~Movie Database~~")
 while True:
+    print("~~Movie Database~~")
     print("1. Insert a new movie")
     print("2. Select a movie")
     print("3. Display all the movies an actor/actress has starred in: ")
-    print("4. Display all the movies directed: ")
+    print("4. Display all the movies directed ")
     print("5. Exit")
     op = int(input("Enter your choice: "))
 
@@ -107,6 +128,6 @@ while True:
         actorDetails(actorName)
     elif op == 4:
         directorName = input("Enter the Director Name:")
-        directorDetails()
+        directorDetails(directorName)
     elif op == 5:
         break
